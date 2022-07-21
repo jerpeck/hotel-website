13d02ce9b6c9e24cd62fa28c66975dc796b5c575
Jeremiah Peck <jerpeck77@gmail.com>
2022-07-20 22:12:43 -0700

    basic apps, urls, views

--- hotel_website/urls.py
+++ hotel_website/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('landing_page.urls')),
+    path('rooms/', include('rooms.urls')),
+    path('spa/', include('spa.urls')),
+    path('restaurant/', include('restaurant.urls'))
 ]
